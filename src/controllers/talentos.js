75525fc42f6121a095a86ee9caa28bbb39bd012e
unknown <contatorodrigoantunes@gmail.com>
2020-04-12 04:29:10 -0300

    altera orderby talento

--- src/controllers/talentos.js
+++ src/controllers/talentos.js
@@ -3,7 +3,7 @@ const connection =  require('../database/connection')
 module.exports = {
     // Lista todos
     async talentos (req, res) {
-        const talentos = await connection("talentos").select("*");
+        const talentos = await connection("talentos").orderBy("time", "desc").select("*");
         return res.json(talentos)
     },
     async removeTalento(req, res) {
